c8293b3e06ce3c007865521e4a8e5bab56f31870
milankatira <milankatira26@gmail.com>
2026-06-07 11:19:00 +0530

    fix: populate note list when ctrl+l is pressed
    Co-Authored-By: AW <273421570+ghl-aw@users.noreply.github.com>

--- main.go
+++ main.go
@@ -94,6 +94,8 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 
 		case "ctrl+l":
+			noteList := listFiles()
+			m.list.SetItems(noteList)
 			m.showListVisible = true
 			return m, nil
 		case "ctrl+n":
